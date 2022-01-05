f5f1083f0ee856b902581e75dfed708218c424f0
kingOf0 <ibrahimbugra1077@gmail.com>
2022-01-05 23:51:23 +0300

    moved main function

--- src/main/kotlin/com/kingOf0/testy/FixedArrayList.kt
+++ src/main/kotlin/com/kingOf0/testy/FixedArrayList.kt
@@ -10,3 +10,23 @@ class FixedArrayList<T>(private val length: Int) : ArrayList<T>() {
     }
 
 }
+
+fun main() {
+
+    val stack = FixedArrayList<Int>(5)
+    stack.add(5)
+    stack.add(3)
+    stack.add(10)
+    stack.add(20)
+    stack.add(8)
+    stack.add(5)
+    stack.add(8)
+    stack.add(1)
+    stack.add(3)
+    println(stack)
+    for (i in stack) {
+        println(i)
+    }
+    println(stack)
+
+}
--- src/main/kotlin/com/kingOf0/testy/Main.kt
+++ src/main/kotlin/com/kingOf0/testy/Main.kt
@@ -1,23 +1,7 @@
 package com.kingOf0.testy
 
 fun main() {
-
-    val stack = FixedArrayList<Int>(5)
-    stack.add(5)
-    stack.add(3)
-    stack.add(10)
-    stack.add(20)
-    stack.add(8)
-    stack.add(5)
-    stack.add(8)
-    stack.add(1)
-    stack.add(3)
-    println(stack)
-    for (i in stack) {
-        println(i)
-    }
-    println(stack)
-
+    
 }
 
 
